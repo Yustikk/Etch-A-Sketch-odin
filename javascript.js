770b59d8f88bf378e0c0399364d2a19a9a2bc7a4
Yustikk <anaiustina99@gmail.com>
2025-05-15 17:36:49 +0300

    added coloring feature

--- javascript.js
+++ javascript.js
@@ -1,20 +1,51 @@
 //creating an nxn grid (n<=100)  gridSize=n
 
 let gridSize=16; 
+let clicked=false;
+let previousColor;
+
+
+
+
+let body = document.querySelector("body")
+body.addEventListener("mouseup", () => clicked=false)
+
+body.addEventListener("selectstart", function(e) {e.preventDefault()})
+        
 
 function createGrid()
 {
-    
-    let container=document.querySelector("#container");
+    let container=document.getElementById("container");
+
+    makeGridSquare();
     for(i=0;i<gridSize;i++)
     {
         
         let row=document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
+        
         for(j=0;j<gridSize;j++)
         {
+
             let column=document.createElement("div");
+            column.addEventListener("mouseenter", function (e){
+                previousColor=getComputedStyle(column).background;
+                let a=e.target;
+                a.style.background="blue";
+            });
+
+            column.addEventListener("mouseleave",function (e) {
+                if(!clicked)
+                e.target.style.background=previousColor;
+            });
+
+            column.addEventListener("mousedown", function (e) {
+                e.target.style.background="red";
+                previousColor="red";
+                clicked=true;
+            })
+            column.addEventListener("mouseup", () => clicked=false );
             column.classList.add("column");
             row.appendChild(column);
         }
@@ -22,15 +53,11 @@ function createGrid()
     }
 }
 
-createGrid();
-
-function makeContainerSquare()
+function makeGridSquare()
 {
-    
-    
-    
+    let container=document.getElementById("container");
+    container.style.height=getComputedStyle(container).width;
 }
 
-makeContainerSquare()
 
-//since this didnt work ill just make alll the inside divs square
+createGrid();
