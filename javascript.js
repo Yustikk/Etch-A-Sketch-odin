fa4fbe94f55e4cd726220d23b9f8079ee56f0f88
Yustikk <anaiustina99@gmail.com>
2025-05-22 16:27:04 +0300

    Added color Picker

--- javascript.js
+++ javascript.js
@@ -8,12 +8,17 @@ let currentColor="red";
 
 
 
+//preventing drag action that messes up the drawing action
 let body = document.querySelector("body")
 body.addEventListener("mouseup", () => clicked=false)
 body.addEventListener("selectstart", function(e) {e.preventDefault()})
 
 
-
+function makeGridSquare()               
+{
+    let container=document.getElementById("container");
+    container.style.height=getComputedStyle(container).width;
+}
         
 
 function createGrid()
@@ -23,30 +28,41 @@ function createGrid()
     makeGridSquare();
     for(i=0;i<gridSize;i++)
     {
-        
+        //adding rows
         let row=document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
         
         for(j=0;j<gridSize;j++)
         {
-
+            //adding every square and adding their listener functions for drawing
             let column=document.createElement("div");
             column.addEventListener("mouseenter", function (e){
+                
                 previousColor=getComputedStyle(column).background;
                 let a=e.target;
                 a.style.background=currentColor;
+                
             });
 
             column.addEventListener("mouseleave",function (e) {
                 if(!clicked)
                 e.target.style.background=previousColor;
+
             });
 
             column.addEventListener("mousedown", function (e) {
+                if(ColorPickerMode){
+                    currentColor=previousColor;
+                    ColorPickerMode=false;
+                    colorpicker.classList.toggle("border");
+                    console.log(currentColor)
+                }
+                else{
                 e.target.style.background=currentColor;
                 previousColor=currentColor;
                 clicked=true;
+                }
             })
             column.addEventListener("mouseup", () => clicked=false );
             column.classList.add("column");
@@ -56,17 +72,12 @@ function createGrid()
     }
 }
 
-function makeGridSquare()
-{
-    let container=document.getElementById("container");
-    container.style.height=getComputedStyle(container).width;
-}
+
 
 function getColor()
 {
     let color=document.getElementById("favcolor");
     color.addEventListener("change",function(e){
-        console.log(e.target.value)
         currentColor=e.target.value;
     })
 }
@@ -84,6 +95,25 @@ let gridsize=document.getElementById("gridSize");
     }
     
 })
+
+let ColorPickerMode=false;
+let ColorPickerModeRightClick=false; 
+let colorpicker=document.getElementById("colorpicker")
+colorpicker.addEventListener("click", (e) => {
+    if(ColorPickerMode)
+    {
+        ColorPickerMode=false
+        colorpicker.classList.toggle("border");
+    }
+    else 
+    {
+        ColorPickerMode=true;
+        colorpicker.classList.toggle("border");
+    }
+});
+
+
+
 
 
 getColor();
